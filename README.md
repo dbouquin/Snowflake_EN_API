2a6925fd548722b6139602d0da4e8b15b0137ee6
dbouquin <dbouquin@gmail.com>
2024-10-02 16:59:05 -0400

    working example using the IMPORTS handler

---
+++ README.md
@@ -0,0 +1,1 @@
+The import file needs to have the campagin page name as a column and "P" for participation as the column value - you can "override" this page name in API settings. You need to upload this page and same it.
--- en_api_test.py
+++ en_api_test.py
@@ -1,13 +1,39 @@
+# import requests
+
+# url = "https://us.engagingnetworks.app/ens/service/authenticate"
+
+# payload = "a8fda2f8-aff5-44e7-be10-6234309f3ce0"
+# headers = {
+#     "Content-Type": "application/json",
+#     "Accept": "application/json"
+# }
+
+# response = requests.post(url, data=payload, headers=headers)
+
+# print(response.json())
+
 import requests
 
-url = "https://us.engagingnetworks.app/ens/service/authenticate"
+# API URL and token
+url = 'https://us.engagingnetworks.app/ea-dataservice/import.service'  # Use the appropriate region URL
+token = '63f77a9d-266b-4063-ac29-d548fa9bfe57'
 
-payload = "a8fda2f8-aff5-44e7-be10-6234309f3ce0"
-headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
+# File and format details
+file_path = '/Users/dbouquin/Library/CloudStorage/OneDrive-NationalParksConservationAssociation/Data_Vault/Snowflake_EN_API/P2P_EN_page_interactions.csv'
+format_name = 'classy_API_pp_v2'
+
+# Payload for the request
+data = {
+    'token': token,
+    'name': 'Test import',
+    'formatName': format_name
 }
 
-response = requests.post(url, data=payload, headers=headers)
+# File to upload
+files = {'upload': open(file_path, 'rb')}
 
-print(response.json())
+# Send the request
+response = requests.post(url, data=data, files=files)
+
+# Print the response
+print(response.text)

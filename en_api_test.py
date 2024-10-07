60bd71e33c0f8b0efbda54a60cbc7c37d798c133
dbouquin <dbouquin@gmail.com>
2024-10-07 17:02:17 -0400

    refactored version to work with stages within new schema

--- en_api_test.py
+++ en_api_test.py
@@ -1,17 +1,3 @@
-# import requests
-
-# url = "https://us.engagingnetworks.app/ens/service/authenticate"
-
-# payload = "a8fda2f8-aff5-44e7-be10-6234309f3ce0"
-# headers = {
-#     "Content-Type": "application/json",
-#     "Accept": "application/json"
-# }
-
-# response = requests.post(url, data=payload, headers=headers)
-
-# print(response.json())
-
 import requests
 
 # API URL and token
